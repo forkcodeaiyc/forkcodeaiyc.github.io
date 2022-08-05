06643c570be6f6e582957aec34f44184d55c5b97
AndersonHJB <bornforthis@bornforthis.cn>
2022-08-05 16:13:36 +0800

    update

--- tommytian57/.vuepress/navbar.ts
+++ tommytian57/.vuepress/navbar.ts
@@ -2,9 +2,4 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
-  {
-    text: "关于田浩良",
-    icon: "people",
-    link: "/intro"
-  }
 ]);
--- tommytian57/.vuepress/theme.ts
+++ tommytian57/.vuepress/theme.ts
@@ -26,7 +26,7 @@ export default hopeTheme({
   lastUpdated: false,
   editLink: false,
 
-  footer: "纽约大学大一在读，热爱摄影，设计，骑行，在学校拥有自己的自行车俱乐部，欢迎大家加入。",
+  // footer: "纽约大学大一在读，热爱摄影，设计，骑行，在学校拥有自己的自行车俱乐部，欢迎大家加入。",
 
   displayFooter: true,
 
